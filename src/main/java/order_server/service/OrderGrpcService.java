41aa7a64885c0d9152a53b37969670a2134a2619
Matheus Saleh <2211600056@fema.edu.br>
2025-06-03 13:32:38 -0300

    incio de criação do grpc service

---
+++ src/main/java/order_server/service/OrderGrpcService.java
@@ -0,0 +1,9 @@
+package order_server.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.grpc.server.service.GrpcService;
+
+@GrpcService
+@RequiredArgsConstructor
+public class OrderGrpcService {
+}
