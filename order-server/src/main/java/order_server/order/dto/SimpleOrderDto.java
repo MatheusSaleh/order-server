1dea0c5f7bf4fd0fa96d2eecb63bb82e96fdac81
Matheus Saleh <2211600056@fema.edu.br>
2025-06-10 09:25:15 -0300

    :sparkles: feat: correcao nas urls e no dto

--- order-server/src/main/java/order_server/order/dto/SimpleOrderDto.java
+++ order-server/src/main/java/order_server/order/dto/SimpleOrderDto.java
@@ -1,8 +1,18 @@
 package order_server.order.dto;
 
+import java.io.Serializable;
 import java.util.List;
 
 public record SimpleOrderDto(
-    String orderId, String customerId, List<SimpleOrderItem> items, String initialStatus) {
-  public record SimpleOrderItem(String productId, int quantity, double price) {}
+        String orderId,
+        String customerId,
+        List<SimpleOrderItem> items,
+        String initialStatus
+) implements Serializable {
+
+  public record SimpleOrderItem(
+          String productId,
+          int quantity,
+          double price
+  ) implements Serializable {}
 }
